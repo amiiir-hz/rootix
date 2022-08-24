b5d444a283d1c46edb1a75907c4501b214cea6af
Saba Beigi <beigisaba@gmail.com>
2022-08-25 03:10:50 +0430

    fix arrow error

--- components/assets/svg/TopArrow.js
+++ components/assets/svg/TopArrow.js
@@ -12,9 +12,9 @@ function TopArrow() {
       <path
         d="M11 6.02124L6 0.999963L1 6.02124"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
