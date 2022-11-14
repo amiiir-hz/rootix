0412df21ed4b35c81943f7a8dca83a77039843fe
Saba Beigi <beigisaba@gmail.com>
2022-11-14 05:20:43 +0330

    switch to typesript

--- pages/test/index.js
+++
@@ -1,4 +0,0 @@
-// import TradingViewWidget from "react-tradingview-widget";
-
-// const Test = () => <TradingViewWidget symbol="NASDAQ:AAPL" />;
-// export default Test;
--- src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import mode from "./modeReducer";
-
-const rootReducer = combineReducers({
-  mode: mode,
-});
-
-export default rootReducer;
